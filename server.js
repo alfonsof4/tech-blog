eb6cf6cf52179519c1ca6f345b0294a3018d60e6
alfonsof4 <alfonsofernandez4th@gmail.com>
2022-02-14 21:38:42 -0600

    having trouble displaying webpage

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connnection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sesh = {
